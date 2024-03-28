466e1c9797e088dad513c9dd90940356aebc2e65
majadey4u <maryjaneogbuju@gmail.com>
2024-03-28 22:00:11 +0100

    linkedin

---
+++ index.js
@@ -0,0 +1,25 @@
+What You're Aiming For
+
+For this activity, you need to fill in all the components and sections provided by LinkedIn, check your SSI, and make a post.
+
+
+Instructions
+
+Create or log in to your LinkedIn profile
+Fill in your profile (* Professional picture, Headline, Summary / About, Featured, Professional experience, Skills, Recommendations )
+Create a relevant and high-quality post.
+Track your SSI before the changes, and then after a week. 
+Compare the results, to know your next steps.
+The final output is a link to your linkedin profile and a comparison of the SSI Results.
+
+
+How We'll Evaluate
+
+Your checkpoint will be evaluated based on these criteria, each rated from 0 to 5.
+
+Checkpoint Assessment
+
+Your Tech Skills
+Quality of Work
+Problem-Solving Skills
+Deadline Alert: (0 score if missed)
